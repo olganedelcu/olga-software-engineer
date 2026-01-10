ad381c238b269a8bec32b93f5fea98055482cfa4
Olga <olganedelcuam@gmail.com>
2026-01-10 19:43:24 +0200

    Update hashnode blog

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ const Footer = () => {
     },
     {
       name: 'Hashnode',
-      url: 'https://hashnode.com/@OlgaSoftware',
+      url: 'https://olgaconstantina.hashnode.dev',
       icon: (
         <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
           <path d="M22.351 8.019l-6.37-6.37a5.63 5.63 0 0 0-7.962 0l-6.37 6.37a5.63 5.63 0 0 0 0 7.962l6.37 6.37a5.63 5.63 0 0 0 7.962 0l6.37-6.37a5.63 5.63 0 0 0 0-7.962zM12 15.953a3.953 3.953 0 1 1 0-7.906 3.953 3.953 0 0 1 0 7.906z"/>
